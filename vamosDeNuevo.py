ba46289c8f5360bdaf068f081b2d97ddd707ad10
Fermin <fgomez@itba.edu.ar>
2020-03-31 15:35:56 -0300

    Creo que ahi anda

--- vamosDeNuevo.py
+++ vamosDeNuevo.py
@@ -1,8 +1,10 @@
+# Arbol de bayes
 parents = {"rank": [], "gre": ["rank"], "gpa": ["rank"], "admit": ["rank", "gre", "gpa"]}  # relaciones de padres
 values = {"admit": [0, 1], "gre": [0, 1], "gpa": [0, 1], "rank": [1, 2, 3, 4]}  # valores posibles de las variables
 nodes = ["admit", "gre", "gpa", "rank"]  # nodos
 
 
+# Normalizacion de la data
 def getData():
     data = []
     with open('./resources/Admision.csv', 'r') as data_file:
@@ -16,6 +18,7 @@ def getData():
             data.append({"admit": admit, "gre": gre, "gpa": gpa, "rank": rank})
     return data
 
+
 def product(*args):
     pools = [tuple(pool) for pool in args]
     result = [[]]
@@ -24,6 +27,7 @@ def product(*args):
     for product in result:
         yield tuple(product)
 
+
 def permuteParentsValues(node):
     aux = {}
     for parent in parents[node]:
@@ -31,7 +35,7 @@ def permuteParentsValues(node):
     return [tuple(v) for v in product(*aux.values())]
 
 
-def conditionalProbabilities(data):
+def getConditionalProbabilities(data):
     # inicializamos diccionarios de frecuencias
     frequencies_nominator = {}
     frequencies_denominator = {}
@@ -40,10 +44,10 @@ def conditionalProbabilities(data):
         for val in values[node]:  # por cada variable del nodo
             tuple = (node, val)
             frequencies_nominator[tuple] = {}
-            for combination in permuteParentsValues(node):
+            for combination in permuteParentsValues(node): # padres **
                 frequencies_nominator[tuple][combination] = 0
                 frequencies_denominator[node][combination] = 0
-
+    # contabilizacion de frecuencias
     for row in data:
         for node_, parents_ in parents.items():
             tupleNode = (node_, row[node_])
@@ -52,61 +56,39 @@ def conditionalProbabilities(data):
                 tupleParents = (*tupleParents, row[parent])
             frequencies_nominator[tupleNode][tupleParents] += 1
             frequencies_denominator[node_][tupleParents] += 1
-
+    # probabilidades condicionales
     probabilities = {}
     for k in frequencies_nominator:
         probabilities[k] = {}
         for v in frequencies_nominator[k]:
-            probabilities[k][v] = float(frequencies_nominator[k][v] + 1) / float(frequencies_denominator[k[0]][v] + 1)
-
+            probabilities[k][v] = float(frequencies_nominator[k][v] + 1) / float(frequencies_denominator[k[0]][v] + 1) # aplico laplace (esta bien hacerlo aca?)
     return probabilities
 
 
-def ej2():
-    probabilities = conditionalProbabilities(getData())
-    print(probabilities)
-    prob = {"admit": 1}
-    condition = {"rank": 2, "gre": 0, "gpa": 1}
-    ret = 1
-    joint = {**prob, ** condition}
-    for node, value in joint.items():
-        tupleNode = (node, value)
-        tupleParents = ()
-        for parent in parents[tupleNode[0]]:
-            tupleParents = (*tupleParents, joint[parent])
-        ret *= probabilities[tupleNode][tupleParents]
-
-
+def probabilidadConjunta(conditionalProbabilities_, nodes_):
+    aux = {}
+    for node, vals in values.items():
+        if node in nodes_:
+            aux[node] = [nodes_[node]]
+        else:
+            aux[node] = vals
+    aux = [dict(zip(aux, v)) for v in product(*aux.values())]
+    ret = 0
+    for d in aux:
+        ret_aux = 1
+        for node, value in d.items():
+            tupleNode = (node, value)
+            tupleParents = ()
+            for parent in parents[node]:
+                tupleParents = (*tupleParents, d[parent])
+            ret_aux *= conditionalProbabilities_[tupleNode][tupleParents]
+        ret += ret_aux
     return ret
 
 
+def probabilidadCondicional(condProb ,prob, condition):
+    return probabilidadConjunta(condProb, {**prob, **condition}) / probabilidadConjunta(condProb, condition)
 
-def calculate(condProb ,prob, condition):
-    if len(prob) == 0:
-        return 1
 
-    probabilities = condProb
-    tupleNode = (list(prob.keys())[0], prob[list(prob.keys())[0]])
-    joint = {**prob, **condition}
-    ret = 1
-
-    for node, value in joint.items():
-        aux = {}
-        tupleNode = (node, value)
-        for parent in parents[tupleNode[0]]:
-            if parent in joint:
-                aux[parent] = []
-                aux[parent].append(joint[parent])
-            else:  # calculamos probabilidad total
-                aux[parent] = values[parent]
-        tupleParents = [tuple(v) for v in product(*aux.values())]
-        total_prob = 0
-        for tp in tupleParents:
-            total_prob += probabilities[tupleNode][tp]
-        ret *= total_prob
-
-    return ret / calculate(probabilities,condition, {})
-
-print(ej2())
-print(calculate(conditionalProbabilities(getData()), {"admit": 1}, { "rank": 2, "gre": 0, "gpa": 1}))
-# print(calculate(conditionalProbabilities(getData()), {"admit": 0}, {"rank": 1}))
+print(probabilidadCondicional(getConditionalProbabilities(getData()), {"admit": 1}, {"rank": 2, "gre": 0, "gpa": 1}))
+print(probabilidadCondicional(getConditionalProbabilities(getData()), {"admit": 0}, {"rank": 1}))
