922816cdc05608b9cb0e11af2b9fad351b89792f
Fermin <fgomez@itba.edu.ar>
2020-03-31 15:40:23 -0300

    Creo que ahi anda

--- vamosDeNuevo.py
+++ vamosDeNuevo.py
@@ -91,4 +91,4 @@ def probabilidadCondicional(condProb ,prob, condition):
 
 
 print(probabilidadCondicional(getConditionalProbabilities(getData()), {"admit": 1}, {"rank": 2, "gre": 0, "gpa": 1}))
-print(probabilidadCondicional(getConditionalProbabilities(getData()), {"admit": 0}, {}))
+print(probabilidadCondicional(getConditionalProbabilities(getData()), {"admit": 0}, {"rank": 1}))
